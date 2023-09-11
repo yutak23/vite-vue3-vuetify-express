d6c1a405eae1f118c11dfb55fe53944f52df1db5
yutak23 <yutapilafu@gmail.com>
2023-09-12 08:47:24 +0900

    feat: フッターを追加

---
+++ src/components/NavigationFooter.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-app-bar color="grey-lighten-2" location="bottom">
+		<v-row>
+			<v-col class="text-center" cols="12">
+				{{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+			</v-col>
+		</v-row>
+	</v-app-bar>
+</template>
+
+<script>
+export default {
+	name: 'NavigationFooter'
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
 	<v-app>
+		<!-- https://vuetifyjs.com/en/features/application-layout/#placing-components に記載がある通り、実装順にレイヤーができる -->
+		<NavigationFooter />
 		<NavigationHeader />
 		<v-main class="bg-grey-lighten-5"><router-view /></v-main>
 	</v-app>
 </template>
 <script>
 import NavigationHeader from '@/components/NavigationHeader.vue';
+import NavigationFooter from '@/components/NavigationFooter.vue';
 
 export default {
 	name: 'HomeView',
-	components: { NavigationHeader }
+	components: { NavigationHeader, NavigationFooter }
 };
 </script>

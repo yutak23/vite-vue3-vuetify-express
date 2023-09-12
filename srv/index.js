277957a29624abbef67473e7b9a8ae7c5b3bddb1
yutak23 <yutapilafu@gmail.com>
2023-09-12 11:36:28 +0900

    feat: SPAをexpressから配信する設定を追加

--- srv/index.js
+++ srv/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import compression from 'compression';
+import history from 'connect-history-api-fallback';
 import chalk from 'chalk';
 import consoleExpressRoutes from 'console-express-routes';
 
 import errorResponse from './lib/error-response.js';
 
 const PORT = 3000;
+const prod = process.env.NODE_ENV === 'production';
 
 const app = express();
 
@@ -13,6 +15,11 @@ app.use(compression({ level: 1, memLevel: 3 }));
 app.use(errorResponse());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (prod) {
+	app.use(history());
+	app.use(express.static('dist'));
+}
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
